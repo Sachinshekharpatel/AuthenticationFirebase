15ca2c2caffa123c15ddb002375d5e930cbe30ff
Sachinshekharpatel <heroft7024@gmail.com>
2024-04-12 13:57:45 +0530

    signupdata are goping in the api through fetch request error are also showing in console

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,97 @@
+import { useState, useContext, useRef } from "react";
+import Button from "react-bootstrap/Button";
+import classes from "./AuthForm.module.css";
+import AuthContext from "../store/auth-context";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    //  api key GOT FORM FIREBASE project setting option = AIzaSyBFjPtZRv42asMyN8Ts4xm18_mYVEaJACA
+    setIsLoading(true);
+
+  let url ;
+    if (isLogin) {
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBFjPtZRv42asMyN8Ts4xm18_mYVEaJACA";
+    } else {
+       url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBFjPtZRv42asMyN8Ts4xm18_mYVEaJACA';
+    }
+    fetch(
+      url,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((response) => {
+      setIsLoading(false);
+      if (response.ok) {
+        console.log(response);
+        return response.json();
+      } else {
+        return response.json().then((data) => {
+          let errorMessage = "Authentication failed!";
+          if (data && data.error && data.error.message) {
+             errorMessage = data.error.message; 
+          }
+          alert(errorMessage);
+         throw new Error(errorMessage);
+        });
+      }
+    }).then((data) => {
+       authCtx.login(data.idToken)      
+    }).catch((error) => {
+      alert(error.message);
+    })
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">
+            Your Password
+          </label>
+          <input type="password" id="password" required  ref={passwordInputRef}/>
+        </div>
+        <div className={classes.actions}>
+          { !isLoading && <button>{isLogin ? "Login" : "Create Account"}</button>}
+          {
+            isLoading && <p style={{color:"white"}}>Sending request...</p>
+          }
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
